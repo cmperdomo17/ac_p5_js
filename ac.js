68205bfcc758c68a695edaa71e0ea7e8f5470a46
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-04-28 15:29:39 -0500

    Dibujar tres lineas abajo del canvas con pixeles aleatorios

---
+++ ac.js
@@ -0,0 +1,34 @@
+import p5 from 'p5';
+
+let sketch = new p5((p) => {
+    let canvasHeight = 600;
+    let canvasWidth = 600;
+
+    p.setup = () => {
+        p.createCanvas(canvasWidth, canvasHeight);
+        p.background(255);
+    }
+
+    p.draw = () => {
+        // Rellenar el canvas con pixeles aleatorios
+
+        // for (let c = 0; c < canvasWidth; c++) {
+        //     for (let f = 0; f < canvasHeight; f++) {
+        //         let pix = p.color(p.random(255), p.random(255), p.random(255));
+        //         p.set(c, f, pix);
+        //     }
+        // }
+        
+        // Dibujar tres lineas abajo del canvas con pixeles aleatorios
+        for (let i = 0; i < 3; i++) {
+            for (let x = 0; x < canvasWidth; x++) {
+                let y = canvasHeight - i - 1; 
+                p.stroke(p.random(255), p.random(255), p.random(255)); 
+                p.point(x, y); 
+            }
+        }
+        p.updatePixels();
+    }
+})
+
+export default sketch;
--- main.js
+++ main.js
@@ -1,26 +1,13 @@
 import './style.css'
 import javascriptLogo from './javascript.svg'
+import sketch from './ac.js'
 import p5 from 'p5'
 
 document.querySelector('#app').innerHTML = `
   <div class="container">
-    <h1> Automatas Celulares with </h1>
-    <img id="logo_js" src="${javascriptLogo}" alt="JavaScript Logo" />
-    <div id="app"></div>
+    <div id="canvas-container"></div>
   </div>
 `
 
-new p5((sketch) => {
-  sketch.setup = () => {
-    sketch.createCanvas(400, 400);
-    sketch.background(0);
-  }
-
-  sketch.draw = () => {
-    sketch.fill(255);
-    sketch.ellipse(sketch.width / 2, sketch.height / 2, 50, 50);
-  }
-}, 'app');
-
-
-
+{/* <h1> Automatas Celulares with </h1>
+<img id="logo_js" src="${javascriptLogo}" alt="JavaScript Logo" /> */}
