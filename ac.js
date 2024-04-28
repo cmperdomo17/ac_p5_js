ebd9ded4daf2a9bd0e54e36229d24f50fdb819ab
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-04-28 15:50:25 -0500

    xd

--- ac.js
+++ ac.js
@@ -1,26 +1,33 @@
 import p5 from 'p5';
 
 let sketch = new p5((p) => {
-    let canvasHeight =  500;
-    let canvasWidth = 500;
-
-    let cols; let rows;
-    let size = 20;
-
+    let canvasHeight = 600;
+    let canvasWidth = 600;
 
     p.setup = () => {
         p.createCanvas(canvasWidth, canvasHeight);
-        cols = canvasHeight/size;
-        rows = canvasWidth/size;
+        p.background(255);
     }
 
     p.draw = () => {
-        p.background(220);
-        for (let i=0; i<cols; i++) {
-            for(let j=0; j<rows; j++) {
-                p.rect(i*size, j*size, size, size);
+        // Rellenar el canvas con pixeles aleatorios
+
+        // for (let c = 0; c < canvasWidth; c++) {
+        //     for (let f = 0; f < canvasHeight; f++) {
+        //         let pix = p.color(p.random(255), p.random(255), p.random(255));
+        //         p.set(c, f, pix);
+        //     }
+        // }
+
+        // Dibujar tres lineas abajo del canvas con pixeles aleatorios
+        for (let i = 0; i < 3; i++) {
+            for (let x = 0; x < canvasWidth; x++) {
+                let y = canvasHeight - i - 1; 
+                p.stroke(p.random(255), p.random(255), p.random(255)); 
+                p.point(x, y); 
             }
         }
+        p.updatePixels();
     }
 })
 
