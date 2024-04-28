fdbd637a5646092f7ed266e1ff0201450a897ea9
Carlos Mario Perdomo Ramos <cmperdomo@unicauca.edu.co>
2024-04-28 16:10:50 -0500

    first version

--- ac.js
+++ ac.js
@@ -1,32 +1,52 @@
 import p5 from 'p5';
 
 let sketch = new p5((p) => {
-    let canvasHeight = 600;
-    let canvasWidth = 600;
+    let canvasHeight = 70;
+    let canvasWidth = 70;
 
     p.setup = () => {
         p.createCanvas(canvasWidth, canvasHeight);
         p.background(255);
     }
 
-    p.draw = () => {
-        // Rellenar el canvas con pixeles aleatorios
-
-        // for (let c = 0; c < canvasWidth; c++) {
-        //     for (let f = 0; f < canvasHeight; f++) {
-        //         let pix = p.color(p.random(255), p.random(255), p.random(255));
-        //         p.set(c, f, pix);
-        //     }
-        // }
-
-        // Dibujar tres lineas abajo del canvas con pixeles aleatorios
+    const drawRandomLines = () => {
+        // Dibujar tres lineas abajo del canvas con pixeles aleatorios en gama de naranja o negros
         for (let i = 0; i < 3; i++) {
             for (let x = 0; x < canvasWidth; x++) {
                 let y = canvasHeight - i - 1; 
-                p.stroke(p.random(255), p.random(255), p.random(255)); 
+                if (p.random() < 0.1) { // 10% de posibilidad de ser negro
+                    p.stroke(0, 0, 0);
+                } else { // 90% de posibilidad de ser naranja
+                    p.stroke(p.random(156, 255), p.random(87, 135), 0);
+                }
                 p.point(x, y); 
             }
         }
+    }
+
+    const drawAverageLines = () => {
+        // Repetir el proceso para cada línea desde la parte inferior hasta la parte superior del lienzo
+        for (let i = 2; i < canvasHeight; i++) {
+            for (let x = 0; x < canvasWidth; x++) {
+                let y = canvasHeight - i; 
+                let colorUp = p.get(x, (y - 1 + canvasHeight) % canvasHeight);
+                let colorDown = p.get(x, (y + 1) % canvasHeight);
+                let colorLeft = p.get((x - 1 + canvasWidth) % canvasWidth, y);
+                let colorRight = p.get((x + 1) % canvasWidth, y);
+                let averageColor = [
+                    (colorUp[0] + colorDown[0] + colorLeft[0] + colorRight[0]) / 4,
+                    (colorUp[1] + colorDown[1] + colorLeft[1] + colorRight[1]) / 4,
+                    (colorUp[2] + colorDown[2] + colorLeft[2] + colorRight[2]) / 4
+                ];
+                p.stroke(averageColor);
+                p.point(x, y - 1); // Dibuja el valor promedio en la línea anterior
+            }
+        }
+    }
+
+    p.draw = () => {
+        drawRandomLines();
+        drawAverageLines();
         p.updatePixels();
     }
 })
