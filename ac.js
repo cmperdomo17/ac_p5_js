1b0e30a1a94c2e1de0adb2464d2045f473acec29
CrisQR <cdqr27@gmail.com>
2024-04-28 15:47:30 -0500

    Grid added

--- ac.js
+++ ac.js
@@ -1,33 +1,26 @@
 import p5 from 'p5';
 
 let sketch = new p5((p) => {
-    let canvasHeight = 600;
-    let canvasWidth = 600;
+    let canvasHeight =  500;
+    let canvasWidth = 500;
+
+    let cols; let rows;
+    let size = 20;
+
 
     p.setup = () => {
         p.createCanvas(canvasWidth, canvasHeight);
-        p.background(255);
+        cols = canvasHeight/size;
+        rows = canvasWidth/size;
     }
 
     p.draw = () => {
-        // Rellenar el canvas con pixeles aleatorios
-
-        // for (let c = 0; c < canvasWidth; c++) {
-        //     for (let f = 0; f < canvasHeight; f++) {
-        //         let pix = p.color(p.random(255), p.random(255), p.random(255));
-        //         p.set(c, f, pix);
-        //     }
-        // }
-        
-        // Dibujar tres lineas abajo del canvas con pixeles aleatorios
-        for (let i = 0; i < 3; i++) {
-            for (let x = 0; x < canvasWidth; x++) {
-                let y = canvasHeight - i - 1; 
-                p.stroke(p.random(255), p.random(255), p.random(255)); 
-                p.point(x, y); 
+        p.background(220);
+        for (let i=0; i<cols; i++) {
+            for(let j=0; j<rows; j++) {
+                p.rect(i*size, j*size, size, size);
             }
         }
-        p.updatePixels();
     }
 })
 
